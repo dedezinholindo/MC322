f26495a8f843b1b99d3a49c28219d19c0ac87c6b
André Ricardo <dedaricardo@gmail.com>
2021-04-17 12:26:32 -0300

    trabaho restaum com Pedro

---
+++ lab04/src/mc322/lab04/Peca.java
@@ -0,0 +1,17 @@
+package mc322.lab04;
+
+public class Peca {
+    
+    int x;
+    int y;
+
+    /*
+    * x: coordenada x de Peca.
+    * y: coordeanda y de Peca.
+    * Inicializa uma Peca.
+     */
+    Peca(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
